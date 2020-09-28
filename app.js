1edd04d69f2db1ceb96a08838af0a428a043770f
Erika Grin <evgeny.grin@iprosis.com>
2020-09-28 22:28:04 +0300

    universal port definition

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 var app = express();
-const port = 9000;
+//const port = 9000;
+const PORT = process.env.PORT || 8080;
 const bodyParser = require('body-parser');
 // web browser driver
 const puppeteer = require('puppeteer');
